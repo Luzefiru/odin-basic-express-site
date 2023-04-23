571c0fd6d423cc2e9ca6daa92d12d6b5800923d0
Christian de Jesus IV <105530193+Luzefiru@users.noreply.github.com>
2023-04-24 00:43:00 +0800

    nit: move code order

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 /* server instance setup */
 const express = require('express');
 const app = express();
-
 const host = 'localhost';
 const port = 8000;
 app.listen(port, host, () => {
   console.log(`Server is now being hosted at: http://${host}:${port}`);
 });
+
 /* always serve public directory */
 app.use(express.static('public'));
+
 /* routers */
 const pagesRouter = require('./routes/pages.js');
 app.use(pagesRouter);
