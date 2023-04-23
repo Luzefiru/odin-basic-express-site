7a0460d0dee5e7d8ac1089bfa666091923581218
Luzefiru <ian.deejay.iv@gmail.com>
2023-04-23 21:14:21 +0800

    nit: move code order

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 /* server instance setup */
 const express = require('express');
 const app = express();
-/* always serve public directory */
-app.use(express.static('public'));
-
-/* routers */
-const pagesRouter = require('./routes/pages.js');
-app.use(pagesRouter);
 
 const host = 'localhost';
 const port = 8000;
 app.listen(port, host, () => {
   console.log(`Server is now being hosted at: http://${host}:${port}`);
 });
+/* always serve public directory */
+app.use(express.static('public'));
+/* routers */
+const pagesRouter = require('./routes/pages.js');
+app.use(pagesRouter);
